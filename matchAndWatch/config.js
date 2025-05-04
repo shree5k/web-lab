1a633cc0b2b771df48e06957d70b5ff024bcd9e9
shreeramkdesigns <shreeramk.designs@gmail.com>
2025-05-04 11:41:48 +0530

    update simple view of matchAndWatch

---
+++ matchAndWatch/config.js
@@ -0,0 +1,7 @@
+const config = {
+    TMDB_API_KEY: window.TMDB_API_KEY,
+    BASE_URL: 'https://api.themoviedb.org/3',
+    IMAGE_BASE_URL: 'https://image.tmdb.org/t/p/w500'
+};
+
+export default config;
