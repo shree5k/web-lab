74aac998c9f0415f9fb6c3afeb6b85fa9e47c478
shreeramkdesigns <shreeramk.designs@gmail.com>
2025-05-04 11:53:11 +0530

    update config.js v2

--- matchAndWatch/config.js
+++ matchAndWatch/config.js
@@ -1,7 +1,9 @@
 const config = {
     TMDB_API_KEY: window.location.hostname === 'localhost' 
         ? 'YOUR_LOCAL_API_KEY' // This will be empty in production
-        : window.env.TMDB_API_KEY, // GitHub Pages uses window.env
+        : window.location.hostname === 'github.io' 
+            ? 'YOUR_GITHUB_PAGES_API_KEY' // Add your GitHub Pages API key here
+            : 'YOUR_OTHER_ENV_API_KEY', // Fallback for other environments
     BASE_URL: 'https://api.themoviedb.org/3',
     IMAGE_BASE_URL: 'https://image.tmdb.org/t/p/w500'
 };
